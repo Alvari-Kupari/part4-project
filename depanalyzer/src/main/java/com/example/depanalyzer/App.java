804e4aeb897df897f705a01f828710f897406940
Alvari <alvari.jk@gmail.com>
2025-06-13 00:55:51 +1200

    created boilerplate for rq3 in a new project

---
+++ depanalyzer/src/main/java/com/example/depanalyzer/App.java
@@ -0,0 +1,13 @@
+package com.example.depanalyzer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
